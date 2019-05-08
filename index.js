42a6e396d8afb0eb1bc1fffb2dc3bcb0778a6b5e
deathkillerank <aniket.g1999@gmail.com>
2019-05-08 22:36:32 +0530

    Starter

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var Blog = mongoose.model("Blog",blogSchema);
 app.get("/",function(req,res){
     res.redirect("/blog");
 });
-
+//Index
 app.get("/blog",function(req,res){
     Blog.find({},function(err,blogData){
         if(!err){
@@ -31,6 +31,32 @@ app.get("/blog",function(req,res){
             console.log(err);
         }
     });    
+});
+//New post
+app.get("/blog/new",function(req,res){
+    res.render("new");
+});
+
+//form create post
+app.post("/create",function(req,res){
+    Blog.create(req.body.post,function(err,post){
+        if(!err){
+            res.redirect("/blog");
+        } else{
+            res.render("new");
+        }
+    });
+});
+
+//view
+app.get("/blog/:id",function(req,res){
+    Blog.findById(req.params.id,function(err,postContent){
+        if(!err){
+            res.render("show",{postContent:postContent});
+        } else{
+            res.redirect("/blog");
+        }
+    });
 });
 
 app.listen(3000,function(){
